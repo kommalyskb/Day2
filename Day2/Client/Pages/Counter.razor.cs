d4a6efc8cc5979d9d381f3e0e9b47f9f637a9ed2
kommalyskb <kommalyskb@users.noreply.github.com>
2020-08-05 19:14:04 +0700

    Making partial class

---
+++ Day2/Client/Pages/Counter.razor.cs
@@ -0,0 +1,24 @@
+﻿using Day2.Shared.Repository;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Day2.Client.Pages
+{
+    public partial class Counter
+    {
+        [Inject] ServiceSimple singleton { get; set; }
+        [Inject] ServiceTrasient transient { get; set; }
+
+        private int currentCount = 0;
+
+        private void IncrementCount()
+        {
+            currentCount++;
+            singleton.Increase();
+            transient.Increase();
+        }
+    }
+}
